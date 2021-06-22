d4c27fc55d680281bb1a77b8d2fac2368d04a08e
debuggerx <dx8917312@163.com>
2021-06-22 12:51:29 +0800

    feat: use update the pinned tab instead of create a new tab.

--- background.js
+++ background.js
@@ -15,7 +15,7 @@
       if (normalTabCount === 1 && chromeTabCount === 0) {
         chrome.tabs.create({ pinned: true, index: 0, active: false });
       } else if (normalTabCount === 0 && chromeTabCount === 1 && tabs[0].pinned) {
-        chrome.tabs.create({ pinned: false, index: 1 });
+        chrome.tabs.update(tabs[0].id, { pinned: false });
       } else if (tabs[0].pinned && (normalTabCount > 1 || chromeTabCount > 1 || (normalTabCount === 0 && tabs[1].url.startsWith('chrome://')))) {
         chrome.tabs.remove(tabs[0].id);
       }
